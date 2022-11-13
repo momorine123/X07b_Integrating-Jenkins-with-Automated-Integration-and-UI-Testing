49f63f1721f517be5658be87753e54635fbe7a9a
yen ning <yenning.lee7@gmail.com>
2022-11-13 22:34:58 +0800

    update access right

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v test php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v 'C:\SIT Year 3 Tri 1\ICT3203\LAB\jenkins-php-selenium-test\\src:/var/www/html' php:7.2-apache
 sleep 1
 set +x
 
