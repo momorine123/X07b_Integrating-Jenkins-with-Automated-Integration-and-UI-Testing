78393d3fec644ee9e28b322255063cc34fcbbb97
yen ning <73540954+momorine123@users.noreply.github.com>
2022-11-16 21:51:17 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v 'C:\SIT Year 3 Tri 1\ICT3203\LAB\jenkins-php-selenium-test\\src:/var/www/html' php:7.2-apache
+
+#docker run -d -p 80:8000 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
+sudo su root
+cd /var/jenkins_home/3X03 && sudo git pull
+sudo chmod +x jenkins/scripts/deploy.sh
+sudo chmod +x jenkins/scripts/kill.sh
+sudo cp -r /var/jenkins_home/3X03 /var/www/html
+sudo service apache2 start
+
 sleep 1
+
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.
