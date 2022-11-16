62a2025b9f4a436b4bbb65113447ab078b16643d
yen ning <yenning.lee7@gmail.com>
2022-11-16 19:31:31 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/jenkins/3X03/X07b_Integrating-Jenkins-with-Automated-Integration-and-UI-Testing/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/3X03:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
